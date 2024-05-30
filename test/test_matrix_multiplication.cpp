63e249781bcb14f7fdda8b7f77b051439fe70485
giorgio <giorgionegro@proton.me>
2024-05-30 12:45:16 +0200

    fuzzy test

--- test/test_matrix_multiplication.cpp
+++ test/test_matrix_multiplication.cpp
@@ -1,6 +1,7 @@
 #include "matrix_multiplication.h"
 #include <vector>
 #include <gtest/gtest.h>
+#include <random>
 
 
 // ######################### Source code of multiplyMatrices in src/matrix_mult
@@ -921,6 +922,114 @@ TEST(ScalarVector, OneScalarTimesNormalVector) {
     ASSERT_EQ(C, expected) << "One scalar times normal vector failed";
 }
 
+// Fuzzy test ********************************************************
+
+TEST(FuzzyTest, FuzzyTest) {
+    int aRows = 2;
+    int aCols = 3;
+    int bCols = 2;
+    auto result_list = std::vector<std::vector<std::vector<int>>>(90000, std::vector<std::vector<int>>(9, std::vector<int>(9, 0)));
+    auto expected_list = std::vector<std::vector<std::vector<int>>>(90000, std::vector<std::vector<int>>(9, std::vector<int>(9, 0)));
+        #include <random>
+    std::random_device rd;
+    for (int i = 0; i < 1000; i++) {
+        std::mt19937 gen(rd());
+        std::uniform_int_distribution<> dis(-100, 100);
+        std::vector<std::vector<int>> A(aRows, std::vector<int>(aCols, 0));
+        std::vector<std::vector<int>> B(aCols, std::vector<int>(bCols, 0));
+        for (int j = 0; j < aRows; j++) {
+            for (int k = 0; k < aCols; k++) {
+                A[j][k] = dis(gen);
+            }
+        }
+        for (int j = 0; j < aCols; j++) {
+            for (int k = 0; k < bCols; k++) {
+                B[j][k] = dis(gen);
+            }
+        }
+        multiplyMatrices(A, B, result_list[i], aRows, aCols, bCols);
+        multiplyMatricesWithoutErrors(A, B, expected_list[i], aRows, aCols, bCols);
+    }
+
+
+    aRows = 1;
+    aCols = 1;
+    bCols = 1;
+
+    for( int i = 1000 ; i < 2000 ; i++){
+        std::mt19937 gen(rd());
+        std::uniform_int_distribution<> dis(-200, 200);
+        std::vector<std::vector<int>> A(aRows, std::vector<int>(aCols, 0));
+        std::vector<std::vector<int>> B(aCols, std::vector<int>(bCols, 0));
+        for (int j = 0; j < aRows; j++) {
+            for (int k = 0; k < aCols; k++) {
+                A[j][k] = dis(gen);
+            }
+        }
+        for (int j = 0; j < aCols; j++) {
+            for (int k = 0; k < bCols; k++) {
+                B[j][k] = dis(gen);
+            }
+        }
+        multiplyMatrices(A, B, result_list[i], aRows, aCols, bCols);
+        multiplyMatricesWithoutErrors(A, B, expected_list[i], aRows, aCols, bCols);
+    }
+
+
+    aRows = 8;
+    aCols = 8;
+    bCols = 8;
+
+    for( int i = 2000 ; i < 5000 ; i++){
+        std::mt19937 gen(rd());
+        std::uniform_int_distribution<> dis(-200, 200);
+        std::vector<std::vector<int>> A(aRows, std::vector<int>(aCols, 0));
+        std::vector<std::vector<int>> B(aCols, std::vector<int>(bCols, 0));
+        for (int j = 0; j < aRows; j++) {
+            for (int k = 0; k < aCols; k++) {
+                A[j][k] = dis(gen);
+            }
+        }
+        for (int j = 0; j < aCols; j++) {
+            for (int k = 0; k < bCols; k++) {
+                B[j][k] = dis(gen);
+            }
+        }
+        multiplyMatrices(A, B, result_list[i], aRows, aCols, bCols);
+        multiplyMatricesWithoutErrors(A, B, expected_list[i], aRows, aCols, bCols);
+    }
+    //let's now fuzzy test random dimensions'
+
+    for (int i = 5000; i < 9000; i++) {
+        std::mt19937 gen(rd());
+        std::uniform_int_distribution<> dis(1, 9);
+        aRows = dis(gen);
+        aCols = dis(gen);
+        bCols = dis(gen);
+        std::vector<std::vector<int>> A(aRows, std::vector<int>(aCols, 0));
+        std::vector<std::vector<int>> B(aCols, std::vector<int>(bCols, 0));
+        for (int j = 0; j < aRows; j++) {
+            for (int k = 0; k < aCols; k++) {
+                A[j][k] = dis(gen);
+            }
+        }
+        for (int j = 0; j < aCols; j++) {
+            for (int k = 0; k < bCols; k++) {
+                B[j][k] = dis(gen);
+            }
+        }
+        multiplyMatrices(A, B, result_list[i], aRows, aCols, bCols);
+        multiplyMatricesWithoutErrors(A, B, expected_list[i], aRows, aCols, bCols);
+    }
+
+
+
+
+    ASSERT_EQ(result_list, expected_list) << "Fuzzy test failed";
+
+
+
+}
 
 
 
